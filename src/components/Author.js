479b822a45042db08afcb521f54a43b97c7acf9a
Katie Louise Snape <KSnape16@gmail.com>
2024-11-09 01:32:09 +0000

    Update Author.js

--- src/components/Author.js
+++ src/components/Author.js
@@ -8,7 +8,7 @@ export default function Author() {
     <div className="bg-black py-0 mt-24 flex justify-center">
     
     <p className="text-sky-400 mr-1 ml-5">Challenge by</p> <a className="text-white hover:text-purple-400 transition-all duration-200" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-    <p className="text-sky-400 mr-1 ml-4">Coded by</p> <a className="text-white hover:text-purple-400 transition-all duration-200" href="https://ksnape34.github.io/Portfolio/" target="_blank" rel="noreferrer">Katie Snape</a>.
+    <p className="text-sky-400 mr-1 ml-4">Coded by</p> <a className="text-white hover:text-purple-400 transition-all duration-200" href="https://www.frontendmentor.io/profile/KSnape34" target="_blank" rel="noreferrer">Katie Snape</a>.
 
     </div>
       
